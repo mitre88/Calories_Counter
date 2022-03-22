65e5d9e82cbe38ebcb15d25340840daf8b55089b
mitre88 <bedr10_capacitacion@hotmail.com>
2022-03-21 21:39:13 -0600

    miercoles re noche

--- js/practice.js
+++ js/practice.js
@@ -1,16 +1,38 @@
-const arrayObjetos=[
-   alumno1={
-         nombre:"juan",
-            edad:20,
-   },
-    alumno2={
-            nombre:"pedro",
-            edad:30,
-    },
-    alumno3={
-            nombre:"jose",
-            edad:40,
-    },
-]
-let repo=arrayObjetos.map(item=>item.edad);
-console.log(repo);
+// const arrayObjetos=[
+//    alumno1={
+//          nombre:"juan",
+//             edad:20,
+//    },
+//     alumno2={
+//             nombre:"pedro",
+//             edad:30,
+//     },
+//     alumno3={
+//             nombre:"jose",
+//             edad:40,
+//     },
+// ]
+// let repo=arrayObjetos.map(item=>item.edad);
+// console.log(repo);
+
+let vegetales = ['Repollo', 'Nabo', 'Rábano', 'Zanahoria']
+console.log(vegetales)
+// ["Repollo", "Nabo", "Rábano", "Zanahoria"]
+
+let elementosEliminados = vegetales.splice(1, 2)
+// ["Nabo", "Rábano"] ==> Lo que se ha guardado en "elementosEliminados"
+
+console.log(vegetales)
+// ["Repollo", "Zanahoria"] ==> Lo que actualmente tiene "vegetales" 
+
+
+
+
+
+
+
+
+
+
+
+
